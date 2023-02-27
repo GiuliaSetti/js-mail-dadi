29efa68618720e109b1e01561ef7f766b6e17dd8
GiuliaSetti <giulia.setti96@gmail.com>
2023-02-27 16:25:38 +0100

    scaffolding + pseudo-code

---
+++ mail/js/script.js
@@ -0,0 +1,30 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+/* 
+
+- creo una array con email di fantasia (totale 6);
+
+- chiedo all'user di inserire la mail nel campo input;
+
+al click del bottone:
+
+- creo una variabile false, (che sia vera solo se la mail corrisponde al valore di un qualsiasi indice dell'array).
+
+- genero un ciclo che controlli che la mail dell'user corrisponda a una di quelle dell'array: controlla quindi che la variabile di accesso sia vera solo se la mail corrisponde al valore di un qualsiasi indice dell'array.
+
+? SE la mail corrisponde a una delle mail dell'array:
+    ° Benvenuto!
+:ALTRIMENTI se la mail non corrisponde:
+    ° Mi spiace, prova con un'altra mail.
+
+*/
+
+
+
+
+
+
