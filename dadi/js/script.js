0125c87cb37571b5c9a4c12e154adc5bcca767e4
GiuliaSetti <giulia.setti96@gmail.com>
2023-02-27 17:25:48 +0100

    scaffolding dadi

---
+++ dadi/js/script.js
@@ -0,0 +1,25 @@
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+/* 
+- Il giocatore tira il dadp.
+
+- Il computer tira il dado.
+
+? SE il numero dell'utente è maggiore di quello del computer:
+    ° Complimenti, hai vinto!
+
+: ALTRIMENTI SE il numero dell'utente è minore di quello del computer:
+    ° Mi spiace, hai perso, ritenta!
+
+: ALTRIMENTI 
+    ° pareggio
+
+*/
+
+
+
+
+
+
